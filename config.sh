f7c1b27103a2da7bce856c9a176d73f97b2e9de8
seachaos <ye.jian.yin@gmail.com>
2020-05-08 19:00:08 -0700

    test for start on raspberry pi

---
+++ config.sh
@@ -0,0 +1,6 @@
+export IMAGE_NAME=seachaos/raspberrypi-jupyter-cpp:python-3.7.0-opencv
+
+export LOCAL_PORT=8888
+
+export CONFIG_FILE=$HOME/docker_jupyter/config
+export NOTEBOOK_FOLDER=$HOME/docker_jupyter/notebook
--- setup_notebook_password.sh
+++ setup_notebook_password.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 source ./config.sh
 
 docker run --rm \
-    -p $LOCAL_PORT:8888 \
     -v $CONFIG_FILE:/home/jupyter/.jupyter/ \
     -v $NOTEBOOK_FOLDER:/notebook/ \
-    -it $IMAGE_NAME
-    # /bin/bash
+    -it $IMAGE_NAME \
+    /bin/bash -c 'su jupyter -- /home/jupyter/modify_password.sh'
