cbc95608b59e5a9520ebbb46c5365ab257ad27a1
KwangSeok Kwon <kskwon02@gmail.com>
2023-05-10 12:35:09 +0900

    solve console redirection with xeus-cling 0.13.0

--- build1-python-3.7.0-basic/build.sh
+++ build1-python-3.7.0-basic/build.sh
@@ -1,2 +1,2 @@
 #docker build . -t docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:python-3.7.16-basic
-docer buildx build --platform=linux/amd64,linux/arm64 -t docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:3.7.16-basic .
+docker buildx build --platform=linux/amd64,linux/arm64 --push -t docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:3.7.16-basic .
--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #export IMAGE_NAME=seachaos/rpi-jupyter-cpp:python-3.7.16-jupyter
-export IMAGE_NAME=docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:3.7.16-jupyter
+#export IMAGE_NAME=docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:3.7.16-jupyter
+export IMAGE_NAME=rpi-jupyter-cpp:xeus-cling-0.13.0-jupyter
 
 export LOCAL_PORT=28888
 
