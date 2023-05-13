798be2d5f6607c5194c1f554f49cbe52ffe8076a
KwangSeok Kwon <kskwon02@gmail.com>
2023-05-14 05:23:53 +0900

    change start_notebook.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #export IMAGE_NAME=seachaos/rpi-jupyter-cpp:python-3.7.16-jupyter
 #export IMAGE_NAME=docker.dev.ust21.kr/kskwon02/rpi-jupyter-cpp:3.7.16-jupyter
-export IMAGE_NAME=registry.s-100.io/kskwon02/rpi-jupyter-cpp:miniconda-py38_23.3.1_0-jupyter
+export IMAGE_NAME=registry.s-100.io/kskwon02/x-jupyter-cpp:miniconda-py38_23.3.1_0-jupyter
 #export IMAGE_NAME=rpi-jupyter-cpp:xeus-cling-0.13.0-jupyter
 
 export LOCAL_PORT=28888
